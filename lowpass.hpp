af3909943a8d4be71807626849359c1a71d2690e
Freddy  Yu <cycleuser@cycleuser.org>
2015-08-29 13:45:55 +0800

    低通道滤波
    2015-08-29 13:45:53

---
+++ lowpass.hpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+
+#define ALPHA 0.001
+//设置每次更改的权重为0.001
+
+
+class LowPass{
+    
+    
+public:
+
+    
+    LowPass(double newx, double newy ,double newz)
+    {
+        x=x+ALPHA*(newx-x);
+        y=y+ALPHA*(newy-y);
+        z=z+ALPHA*(newz-z);
+    }
+    //在构造函数中对新的输入值进行获取
+    
+    setter（double newx, double newy ,double newz）
+    {
+        x=newx;
+        y=newy;
+        z=newz;
+    }
+    //在设备稳定不动后设定最后一组数值为固定值，留待下一次作为初始值使用
+    
+    
+    double x=0;
+    double y=0;
+    double z=0;
+    //初始值设为0
+};
+
+
