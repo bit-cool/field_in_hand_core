090561b5c129e2d0f2d02f6de44272e7a71bb99a
Freddy  Yu <cycleuser@cycleuser.org>
2015-08-29 14:04:48 +0800

    通过重力加速度计算水平仪图像投点位置
    2015-08-29 14:04:46

---
+++ levelplan.hpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+
+
+
+
+
+class LevelPlain{
+    
+    
+public:
+
+    
+    
+    LevelPlain(float gx,float gy,float gz)
+    {
+        
+        length=2*sqrt(gx*gx+gy*gy+gz*gz);
+        
+        a = gx+length/2;
+        b = -gy+length/2;
+        
+    }
+    //在构造函数中通过重力传感器或者加速度传感器的值计算水平仪的半径和投点的位置
+    
+    
+    
+    
+    float length=0;
+    float x=0;
+    float y=0;
+    
+    
+};
+
+
